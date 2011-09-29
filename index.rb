aeb8f6f6cb032ddb249d7a0d29d99955fbd5c25d
Steven Klise <steven@stevenklise.com>
2011-09-28 22:09:04 -0400

    Now the page links actually work

--- index.rb
+++ index.rb
@@ -1,4 +1,4 @@
 # #### _03 Introduction to Ruby and HTTP_
 
-# * [Sinatra Tutorial](/simple_sinatra/simple_sinatra.html)
-# * [Example Sinatra app created in class](/example_sinatra/app.html)
+# * [Sinatra Tutorial](/03-Intro-To-Ruby---HTTP/simple_sinatra/simple_sinatra.html)
+# * [Example Sinatra app created in class](/03-Intro-To-Ruby---HTTP/example_sinatra/app.html)
